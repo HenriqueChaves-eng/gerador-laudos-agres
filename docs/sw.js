f25f79d8789d5a9d856585874eb65db11c421765
HenriqueChaves-eng <henriqueabramant1@gmail.com>
2026-05-19 16:25:02 -0300

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "agres-pages-offline-v18";
+const CACHE_NAME = "agres-pages-offline-v19";
 const ASSETS = [
   "./",
   "./index.html",
--- static/offline/sw.js
+++ static/offline/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "agres-offline-v22";
+const CACHE_NAME = "agres-offline-v23";
 const ASSETS = [
   "./",
   "./index.html",
