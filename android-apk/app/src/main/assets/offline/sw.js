cf995bfc01111d8a3f78658f54693d53393d1a17
HenriqueChaves-eng <henriqueabramant1@gmail.com>
2026-07-21 16:17:55 -0300

    Add files via upload

--- android-apk/app/src/main/assets/offline/sw.js
+++ android-apk/app/src/main/assets/offline/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "agres-pages-offline-v56";
+const CACHE_NAME = "agres-pages-offline-v57";
 const ASSETS = [
   "./",
   "./index.html",
--- static/coleta/sw.js
+++ static/coleta/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "agres-coleta-v53";
+const CACHE_NAME = "agres-coleta-v54";
 const ASSETS = [
   "./",
   "./index.html",
